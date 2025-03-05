c5763995fe04e749539e44ec12b46df815eb73d1
Justin Lam <justinflam9225k@gmail.com>
2025-03-05 15:58:16 -0800

    Animated the cube

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 import * as THREE from "three";	// a namespace import - creates namespace object THREE containing all exports (due to *) from the module "three"
 
-main();
+let canvas;
+let renderer;
+let scene;
+let camera;
+
+let cube;
 
 function main() {
-	const canvas = document.querySelector("#canvas");
-	const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
-	const scene = new THREE.Scene();
+	initGlobalVars();
 
-	const fov = 75;
-	const aspect = 2;	// default canvas is 300x150 so its aspect is 2 (300/150)
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);	// defaults to looking down the -Z axis with +Y up
 	camera.position.z = 2;	// back up so we can see the origin
 
 	const boxWidth = 1;
@@ -19,9 +17,37 @@ function main() {
 	const boxDepth = 1;
 	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 	const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
-	const cube = new THREE.Mesh(geometry, material);
-
+	cube = new THREE.Mesh(geometry, material);
 	scene.add(cube);
 
-	renderer.render(scene, camera);
+	requestAnimationFrame(tick);
 }
+
+function initGlobalVars() {
+	canvas = document.querySelector("#canvas");
+	renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+	scene = new THREE.Scene();
+
+	const fov = 75;
+	const aspect = 2;	// default canvas is 300x150 so its aspect is 2 (300/150)
+	const near = 0.1;
+	const far = 5;
+	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);	// defaults to looking down the -Z axis with +Y up
+}
+
+/** @param {number} time end time of the previous frame (in ms) */
+function tick(time) {
+	time /= 1000;
+
+	animateCube(time, time);
+	renderer.render(scene, camera);
+   
+	requestAnimationFrame(tick);
+}
+
+function animateCube(xRotation, yRotation) {
+	cube.rotation.x = xRotation;
+	cube.rotation.y = yRotation;
+}
+
+main();
